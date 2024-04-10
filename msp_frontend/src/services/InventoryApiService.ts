16005c3856418956f70832793ce60af7a64712a3
Nico Meyer <nico.meyer@neuland-bfi.de>
2024-04-10 13:04:23 +0200

    20 - ADD service implemented

---
+++ msp_frontend/src/services/InventoryApiService.ts
@@ -0,0 +1,67 @@
+import {Endpoint} from "../util/Endpoint";
+import {InventoryAction} from "../util/InventoryAction";
+import {RequestMethod} from "../util/RequestMethod";
+
+class InventoryApiService {
+
+  public static async getAllItems(): Promise<number> {
+    return await fetch(Endpoint.ITEMS, {
+      method: RequestMethod.GET,
+    }).then(async (response) => {
+      return response.status;
+    })
+  }
+
+  public static async addItem(item: GenericItem, amount: number): Promise<number> {
+    return await fetch(Endpoint.INVENTORY + InventoryAction.ADD, {
+      method: RequestMethod.POST,
+      body: JSON.stringify({item, amount}),
+    }).then(async (response) => {
+      return response.status;
+    })
+  }
+
+  public static async removeItem(item: GenericItem, amount: number): Promise<number> {
+    return await fetch(Endpoint.INVENTORY + InventoryAction.REMOVE, {
+      method: RequestMethod.DELETE,
+      body: JSON.stringify({item, amount}),
+    }).then(async (response) => {
+      return response.status;
+    })
+  }
+
+  public static async getSize(): Promise<number> {
+    return await fetch(Endpoint.INVENTORY + InventoryAction.INV_SIZE, {
+      method: RequestMethod.GET,
+    }).then(async (response) => {
+      return response.status;
+    })
+  }
+
+  public static async setSize(size: number): Promise<number> {
+    return await fetch(Endpoint.INVENTORY + InventoryAction.INV_SIZE, {
+      method: RequestMethod.POST,
+      body: JSON.stringify(size),
+    }).then(async (response) => {
+      return response.status;
+    })
+  }
+
+  public static async isNotFull(): Promise<number> {
+    return await fetch(Endpoint.INVENTORY + InventoryAction.IS_NOT_FULL, {
+      method: RequestMethod.GET,
+    }).then(async (response) => {
+      return response.status;
+    })
+  }
+
+  public static async isItemPresent(item: GenericItem): Promise<number> {
+    return await fetch(Endpoint.INVENTORY + InventoryAction.IS_ITEM_PRESENT, {
+      method: RequestMethod.GET,
+      body: JSON.stringify(item)
+    }).then(async (response) => {
+      return response.status;
+    })
+  }
+
+}
---
+++ msp_frontend/src/util/Endpoint.ts
@@ -0,0 +1,6 @@
+export enum Endpoint {
+  API = "/api",
+  PLAYER = API + "/player",
+  INVENTORY = PLAYER + "/inventory",
+  ITEMS = INVENTORY + "/items"
+}
