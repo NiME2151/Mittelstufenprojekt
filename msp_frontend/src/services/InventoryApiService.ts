f9d9b765fd90c882883cc32ff5cb7424bffd1d32
Nico Meyer <nico.meyer@neuland-bfi.de>
2024-04-11 10:37:00 +0200

    20 - CHANGE fix

--- msp_frontend/src/services/InventoryApiService.ts
+++ msp_frontend/src/services/InventoryApiService.ts
@@ -4,11 +4,13 @@ import {RequestMethod} from "../util/RequestMethod";
 
 class InventoryApiService {
 
-  public static async getAllItems(): Promise<number> {
+  public static async getAllItems(): Promise<GenericItem> {
     return await fetch(Endpoint.ITEMS, {
       method: RequestMethod.GET,
     }).then(async (response) => {
-      return response.status;
+      return response.json();
+    }).then((response) => {
+      return response;
     })
   }
 
@@ -16,7 +18,7 @@ class InventoryApiService {
     return await fetch(Endpoint.INVENTORY + InventoryAction.ADD, {
       method: RequestMethod.POST,
       body: JSON.stringify({item, amount}),
-    }).then(async (response) => {
+    }).then(async (response: Response) => {
       return response.status;
     })
   }
@@ -25,7 +27,7 @@ class InventoryApiService {
     return await fetch(Endpoint.INVENTORY + InventoryAction.REMOVE, {
       method: RequestMethod.DELETE,
       body: JSON.stringify({item, amount}),
-    }).then(async (response) => {
+    }).then(async (response: Response) => {
       return response.status;
     })
   }
@@ -33,8 +35,10 @@ class InventoryApiService {
   public static async getSize(): Promise<number> {
     return await fetch(Endpoint.INVENTORY + InventoryAction.INV_SIZE, {
       method: RequestMethod.GET,
-    }).then(async (response) => {
-      return response.status;
+    }).then(async (response: Response) => {
+      return response.json();
+    }).then((response: number) => {
+      return response;
     })
   }
 
@@ -42,25 +46,29 @@ class InventoryApiService {
     return await fetch(Endpoint.INVENTORY + InventoryAction.INV_SIZE, {
       method: RequestMethod.POST,
       body: JSON.stringify(size),
-    }).then(async (response) => {
+    }).then(async (response: Response) => {
       return response.status;
     })
   }
 
-  public static async isNotFull(): Promise<number> {
+  public static async isNotFull(): Promise<boolean> {
     return await fetch(Endpoint.INVENTORY + InventoryAction.IS_NOT_FULL, {
       method: RequestMethod.GET,
-    }).then(async (response) => {
-      return response.status;
+    }).then(async (response: Response) => {
+      return response.json();
+    }).then((response: boolean) => {
+      return response;
     })
   }
 
-  public static async isItemPresent(item: GenericItem): Promise<number> {
+  public static async isItemPresent(item: GenericItem): Promise<boolean> {
     return await fetch(Endpoint.INVENTORY + InventoryAction.IS_ITEM_PRESENT, {
       method: RequestMethod.GET,
       body: JSON.stringify(item)
-    }).then(async (response) => {
-      return response.status;
+    }).then(async (response: Response) => {
+      return response.json();
+    }).then((response: boolean) => {
+      return response;
     })
   }
 
