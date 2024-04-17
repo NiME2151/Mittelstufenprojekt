2d17202f1f9177d5729b8ba982f783791f49e821
Nico Meyer <nico.meyer@neuland-bfi.de>
2024-04-17 09:05:12 +0200

    72 - CHANGE fix

--- msp_frontend/src/enums/CharacterAction.ts
+++ msp_frontend/src/enums/CharacterAction.ts
@@ -3,5 +3,5 @@ export enum CharacterAction {
   REMOVE_MONEY = "/money/remove",
   EAT = "/eat",
   BUY_ITEM_FROM_TRADER = "/buy_item_from_trader",
-  SELL_ITEM_FROM_TRADER = "/sell_item_from_trader",
+  SELL_ITEM_TO_TRADER = "/sell_item_to_trader",
 }
