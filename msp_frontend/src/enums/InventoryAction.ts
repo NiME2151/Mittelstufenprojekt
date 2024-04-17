5e2b885c50f4daab06f22c9776d05efd8aa15d6b
Nico Meyer <nico.meyer@neuland-bfi.de>
2024-04-17 08:52:41 +0200

    72 - ADD implemented CharacterApiService

---
+++ msp_frontend/src/enums/InventoryAction.ts
@@ -0,0 +1,9 @@
+export enum InventoryAction {
+  ADD = "/add",
+  REMOVE = "/remove",
+  INVENTORY_SIZE = "/size",
+  IS_NOT_FULL = "/is_not_full",
+  IS_ITEM_PRESENT = "/is_item_present",
+  GET_ITEMS_OF_TYPE = "/get_items_of_type",
+  GET_EMPTY_SLOTS = "/get_empty_slots"
+}
--- msp_frontend/src/util/InventoryAction.ts
+++
@@ -1,7 +0,0 @@
-export enum InventoryAction {
-  ADD = "/add",
-  REMOVE = "/remove",
-  INV_SIZE = "/size",
-  IS_NOT_FULL = "/is_not_full",
-  IS_ITEM_PRESENT = "/is_item_present"
-}
