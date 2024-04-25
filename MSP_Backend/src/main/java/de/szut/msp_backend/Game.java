ae66482ddfcc65e5a37c61f031ee5ae4324e856f
Patrick Schewe <87083544+Crackminer@users.noreply.github.com>
2024-04-25 14:19:31 +0200

    Update Game.java

--- MSP_Backend/src/main/java/de/szut/msp_backend/Game.java
+++ MSP_Backend/src/main/java/de/szut/msp_backend/Game.java
@@ -12,37 +12,53 @@ import java.util.Objects;
 @Data 
 public class Game
 {
-  private final Map map;
-  private final Character player;
-  private int clicks;
-  private static ArrayList<Trader> trader;
-  private static Game instance;
-
-  public Game()
-  {
-    map = new Map();
-    trader = new ArrayList<>();
-    //TODO: hier einmal Frontendmann abfragen für Name und co erstellen lassen
-    player = new Character();
-    clicks = 0;
-  }
-
-  public static Game getInstance()
-  {
-    if (instance == null)
+    private final Map map;
+    private final Character player;
+    private int clicks;
+    private static ArrayList<Trader> trader;
+    private static Game instance;
+  
+    public Game()
     {
-      instance = new Game();
+        map = new Map();
+        trader = new ArrayList<>();
+        //TODO: hier einmal Frontendmann abfragen für Name und co erstellen lassen
+        player = new Character();
+        clicks = 0;
     }
-    return instance;
-  }
-
-  public static Trader getTraderById(String traderID)
-  {
-      return trader.stream().filter(t -> Objects.equals(t.getTraderID().toString(), traderID)).findAny().orElseThrow();
-  }
-
-  public void parseGameAction(GameAction gameAction)
-  {
-    clicks += gameAction.doAction(clicks);
-  }
+  
+    public static Game getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Game();
+        }
+        return instance;
+    }
+  
+    
+    public Map getMap()
+    {
+        return map;
+    }
+  
+    public Character getPlayer()
+    {
+        return this.player;
+    }
+  
+    public int getClicks()
+    {
+        return this.clicks;
+    }
+  
+    public static Trader getTraderById(String traderID)
+    {
+        return trader.stream().filter(t -> Objects.equals(t.getTraderID().toString(), traderID)).findAny().orElseThrow();
+    }
+  
+    public void parseGameAction(GameAction gameAction)
+    {
+        clicks += gameAction.doAction(clicks);
+    }
 }
