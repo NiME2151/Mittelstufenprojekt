c2950857958c590a4a2847cd2915ea89dc7844a8
Crackminer <schrewrepatrick@gmail.com>
2024-05-02 10:51:39 +0200

    88 - CHANGE MapController, should be finished now

--- MSP_Backend/src/main/java/de/szut/msp_backend/controller/MapController.java
+++ MSP_Backend/src/main/java/de/szut/msp_backend/controller/MapController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
@@ -17,12 +16,10 @@ public class MapController
 {
   public static Map map;
 
-  @RequestMapping("/current")
+  @GetMapping("/current_node")
   public ResponseEntity<Node> getPlayerNode()
   {
     final Node playerLocation = map.getPlayerLocation();
     return ResponseEntity.status(HttpStatus.OK).body(playerLocation);
   }
-
-  @RequestMapping("/current/description")
 }
