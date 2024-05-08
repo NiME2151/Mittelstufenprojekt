8e6060d906951d3ddecc16eb9b6967ab4c04525f
Nico Meyer <nico.meyer@neuland-bfi.de>
2024-05-08 10:44:25 +0200

    98 - ADD Startmenü implementiert

--- msp_frontend/src/App.js
+++ msp_frontend/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import React from "react";
-import {Test} from "./components/Test";
+import {GameMenu} from "./components/GameMenu";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Trader from "./components/Trader";
+import Game from "./components/Game";
 
 function App() {
   return (
-    <Test />
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<GameMenu />}></Route>
+        <Route path={"/trade"} element={<Trader />}></Route>
+        <Route path={"/game"} element={<Game />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
