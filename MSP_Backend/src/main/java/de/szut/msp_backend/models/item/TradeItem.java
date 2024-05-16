924d67074597610c0495b90aa506c1b26f59d010
Neele Reiners <nreiners@proton.me>
2024-05-16 08:59:55 +0200

    add comments for item

--- MSP_Backend/src/main/java/de/szut/msp_backend/models/item/TradeItem.java
+++ MSP_Backend/src/main/java/de/szut/msp_backend/models/item/TradeItem.java
@@ -10,6 +10,11 @@ public class TradeItem
     private int buyValue;
     private int itemID;
 
+    /**
+     * A special item that is a part of the trading system
+     * with extra parameters like sell value and buy value.
+     * @param item with basic data.
+     */
     public TradeItem(GenericItem item)
     {
         this.sellValue = fillSellValue(item);
@@ -17,10 +22,21 @@ public class TradeItem
         this.itemID = item.getItemID();
     }
 
+    /**
+     * Sets the value with which in can be sold.
+     * @param item with basic data.
+     * @return the sell value that is set in the class trade.
+     */
     public int fillSellValue(GenericItem item)
     {
         return Trade.getSellValue(item);
     }
+
+    /**
+     * Sets the value with which the item can be bought.
+     * @param item with basic data.
+     * @return the buy value that is set in the class trade.
+     */
     public int fillBuyValue(GenericItem item)
     { 
         return Trade.getBuyValue(item); 
--- MSP_Backend/src/main/java/de/szut/msp_backend/models/item/Weapon.java
+++ MSP_Backend/src/main/java/de/szut/msp_backend/models/item/Weapon.java
@@ -11,6 +11,15 @@ public class Weapon extends GenericItem
 {
     private final int damage;
 
+    /**
+     * Creates a weapon which adds attackpoints to the players strength.
+     * @param itemID unique ID of the Item.
+     * @param displayName that the player sees while playing.
+     * @param description for the player, to know what this item can do.
+     * @param standardValue price if the player wants to sell the item or buy it.
+     * @param rarity plays part in how often the item will appear in the game.
+     * @param damage that gets added to the plyers strength in a fight.
+     */
     public Weapon(int itemID, String displayName, String description, int standardValue, Rarity rarity, int damage)
     {
         super(itemID, displayName, description, standardValue, ItemType.WEAPON, rarity);
