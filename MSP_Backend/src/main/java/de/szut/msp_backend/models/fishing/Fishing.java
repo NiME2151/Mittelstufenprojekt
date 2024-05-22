fb438fbf5c5184a5565e377293bea25375f402bf
christhomas <133003458+chrisbaluthom@users.noreply.github.com>
2024-05-22 08:38:57 +0200

    101 - merge main

--- MSP_Backend/src/main/java/de/szut/msp_backend/models/fishing/Fishing.java
+++ MSP_Backend/src/main/java/de/szut/msp_backend/models/fishing/Fishing.java
@@ -9,23 +9,43 @@ public class Fishing
   private static Game game = Game.getInstance();
   private static de.szut.msp_backend.models.map.Map map = game.getMap();
 
-  public static void tryFishing(Character character)
+  public static void fishing(Character character)
   {
-    if (map.getPlayerLocation() == map.lake)
-    {
-
-    }
+      if(map.getPlayerLocation() == map.lake)
+      {
+          switch (rndNumber(0, 10))
+          {
+              case 0:
+                  break;
+              case 1:
+                  break;
+              case 2:
+                  break;
+              case 3:
+                  break;
+              case 4:
+                  break;
+              case 5:
+                  break;
+              case 6:
+                  break;
+              case 7:
+                  break;
+              case 8:
+                  break;
+              case 9:
+                  break;
+          }
+      }
   }
 
   /**
-   * This gives you a random number in between min (inclusive) and max (exclusive)
-   *
+   * This function returns a random number in between min (inclusive) and max (exclusive)
    * @param min
    * @param max
-   *
    * @return random Int
    */
-  private int rndNumber(int min, int max)
+  private static int rndNumber(int min, int max)
   {
     Random r = new Random();
     return r.nextInt((max) - min) + min;
