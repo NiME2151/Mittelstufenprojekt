be3a9472067ee4018bf0656d473ae407bcca6bc4
christhomas <133003458+chrisbaluthom@users.noreply.github.com>
2024-05-22 09:20:12 +0200

    101 - Add fish to characters inventory by fishing

--- MSP_Backend/src/main/java/de/szut/msp_backend/models/fishing/Fishing.java
+++ MSP_Backend/src/main/java/de/szut/msp_backend/models/fishing/Fishing.java
@@ -1,6 +1,7 @@
 package de.szut.msp_backend.models.fishing;
 
 import de.szut.msp_backend.Game;
+import de.szut.msp_backend.models.character.Character;
 
 import java.util.Random;
 
@@ -9,6 +10,10 @@ public class Fishing
   private static Game game = Game.getInstance();
   private static de.szut.msp_backend.models.map.Map map = game.getMap();
 
+    /**
+     *This function adds a random fish to the character's inventory
+     * @param character
+     */
   public static void fishing(Character character)
   {
       if(map.getPlayerLocation() == map.lake)
@@ -16,24 +21,36 @@ public class Fishing
           switch (rndNumber(0, 10))
           {
               case 0:
+                  character.addItemToInventory(getGenericItemById(35));
                   break;
               case 1:
+                  character.addItemToInventory(getGenericItemById(36));
                   break;
               case 2:
+                  character.addItemToInventory(getGenericItemById(37));
                   break;
               case 3:
+                  character.addItemToInventory(getGenericItemById(38));
                   break;
               case 4:
+                  character.addItemToInventory(getGenericItemById(39));
                   break;
               case 5:
+                  character.addItemToInventory(getGenericItemById(40));
                   break;
               case 6:
+                  character.addItemToInventory(getGenericItemById(41));
                   break;
               case 7:
+                  character.addItemToInventory(getGenericItemById(42));
                   break;
               case 8:
+                  character.addItemToInventory(getGenericItemById(43));
                   break;
               case 9:
+                  character.addItemToInventory(getGenericItemById(44));
+                  break;
+              default:
                   break;
           }
       }
