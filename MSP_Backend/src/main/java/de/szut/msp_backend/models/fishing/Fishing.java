422acd7a77a76e5bafca1dbee3cb996bd1608320
christhomas <133003458+chrisbaluthom@users.noreply.github.com>
2024-05-22 11:54:25 +0200

    101 - Merged with main & fixed issues resulting from it

--- MSP_Backend/src/main/java/de/szut/msp_backend/models/fishing/Fishing.java
+++ MSP_Backend/src/main/java/de/szut/msp_backend/models/fishing/Fishing.java
@@ -1,9 +1,12 @@
 package de.szut.msp_backend.models.fishing;
 
 import de.szut.msp_backend.Game;
+import de.szut.msp_backend.exceptions.ItemNotFoundException;
 import de.szut.msp_backend.models.character.Character;
 
 import java.util.Random;
+
+import static de.szut.msp_backend.parser.ItemParser.getGenericItemById;
 
 public class Fishing
 {
@@ -12,43 +15,43 @@ public class Fishing
 
     /**
      *This function adds a random fish to the character's inventory
-     * @param character
+     * @param character 
      */
-  public static void fishing(Character character)
+  public static void fishing(Character character) throws ItemNotFoundException
   {
       if(map.getPlayerLocation() == map.lake)
       {
           switch (rndNumber(0, 10))
           {
               case 0:
-                  character.addItemToInventory(getGenericItemById(35));
+                  character.addItemToInventory(getGenericItemById(35), 1);
                   break;
               case 1:
-                  character.addItemToInventory(getGenericItemById(36));
+                  character.addItemToInventory(getGenericItemById(36), 1);
                   break;
               case 2:
-                  character.addItemToInventory(getGenericItemById(37));
+                  character.addItemToInventory(getGenericItemById(37), 1);
                   break;
               case 3:
-                  character.addItemToInventory(getGenericItemById(38));
+                  character.addItemToInventory(getGenericItemById(38), 1);
                   break;
               case 4:
-                  character.addItemToInventory(getGenericItemById(39));
+                  character.addItemToInventory(getGenericItemById(39), 1);
                   break;
               case 5:
-                  character.addItemToInventory(getGenericItemById(40));
+                  character.addItemToInventory(getGenericItemById(40), 1);
                   break;
               case 6:
-                  character.addItemToInventory(getGenericItemById(41));
+                  character.addItemToInventory(getGenericItemById(41), 1);
                   break;
               case 7:
-                  character.addItemToInventory(getGenericItemById(42));
+                  character.addItemToInventory(getGenericItemById(42), 1);
                   break;
               case 8:
-                  character.addItemToInventory(getGenericItemById(43));
+                  character.addItemToInventory(getGenericItemById(43), 1);
                   break;
               case 9:
-                  character.addItemToInventory(getGenericItemById(44));
+                  character.addItemToInventory(getGenericItemById(44), 1);
                   break;
               default:
                   break;
@@ -58,8 +61,8 @@ public class Fishing
 
   /**
    * This function returns a random number in between min (inclusive) and max (exclusive)
-   * @param min
-   * @param max
+   * @param min int
+   * @param max int
    * @return random Int
    */
   private static int rndNumber(int min, int max)
