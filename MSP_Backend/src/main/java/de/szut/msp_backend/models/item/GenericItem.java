0364df679a510efa24336d017c3b4ff17655403e
christhomas <133003458+chrisbaluthom@users.noreply.github.com>
2024-05-23 12:07:07 +0200

    Added Comments

--- MSP_Backend/src/main/java/de/szut/msp_backend/models/item/GenericItem.java
+++ MSP_Backend/src/main/java/de/szut/msp_backend/models/item/GenericItem.java
@@ -17,7 +17,15 @@ public abstract class GenericItem
   protected int standardValue;
   protected ItemType itemType;
   protected Rarity rarity;
-
+    /**
+     * Creates a generic item.
+     * @param itemID unique ID of the Item.
+     * @param displayName that the player sees while playing.
+     * @param description for the player, to know what this item can do.
+     * @param standardValue price if the player wants to sell the item or buy it.
+     * @param itemType consumable, weapon or tradeItem
+     * @param rarity plays part in how often the item will appear in the game.
+     */
     protected GenericItem(int itemID, String displayName, String description, int standardValue, ItemType itemType, Rarity rarity)
     {
         this.itemID = itemID;
--- MSP_Backend/src/main/java/de/szut/msp_backend/models/item/Item.java
+++ MSP_Backend/src/main/java/de/szut/msp_backend/models/item/Item.java
@@ -10,6 +10,14 @@ import static de.szut.msp_backend.models.item.ItemType.ITEM;
 @EqualsAndHashCode(callSuper = true)
 public class Item extends GenericItem
 {
+    /**
+     * Creates an item.
+     * @param itemId unique ID of the Item.
+     * @param displayName that the player sees while playing.
+     * @param description for the player, to know what this item can do.
+     * @param standardValue price if the player wants to sell the item or buy it.
+     * @param rarity plays part in how often the item will appear in the game.
+     */
     public Item(int itemId, String displayName, String description, int standardValue, Rarity rarity)
     {
         super(itemId, displayName, description, standardValue, ITEM, rarity);
