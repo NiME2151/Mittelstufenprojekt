7d9c40da51a50c265908ff76ae592e0c57768c56
Crackminer <87083544+Crackminer@users.noreply.github.com>
2024-05-25 19:30:20 +0200

    106 - ADD tests for some files, ADD jacoco-maven-plugin for integration of test-coverage for sonarqube

--- MSP_Backend/src/test/java/de/szut/msp_backend/events/GameActionTest.java
+++ MSP_Backend/src/test/java/de/szut/msp_backend/events/GameActionTest.java
@@ -2,10 +2,27 @@ package de.szut.msp_backend.events;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GameActionTest
 {
     @Test
     void doAction()
     {
+        final GameActionImpl gameActionImpl = new GameActionImpl();
+
+        assertEquals(2, gameActionImpl.doAction(2));
+        assertEquals(7, gameActionImpl.doAction(7));
+        assertEquals(2555, gameActionImpl.doAction(2555));
+        assertEquals(-7980, gameActionImpl.doAction(-7980));
+    }
+}
+
+class GameActionImpl implements GameAction
+{
+    @Override
+    public int doAction(final int clicks)
+    {
+        return clicks;
     }
 }
--- MSP_Backend/src/test/java/de/szut/msp_backend/events/PickUpItemGameActionTest.java
+++ MSP_Backend/src/test/java/de/szut/msp_backend/events/PickUpItemGameActionTest.java
@@ -1,5 +1,9 @@
 package de.szut.msp_backend.events;
 
+import de.szut.msp_backend.Game;
+import de.szut.msp_backend.models.item.GenericItem;
+import de.szut.msp_backend.models.item.ItemType;
+import de.szut.msp_backend.models.item.Rarity;
 import org.junit.jupiter.api.Test;
 
 public class PickUpItemGameActionTest
@@ -7,10 +11,21 @@ public class PickUpItemGameActionTest
     @Test
     void doAction()
     {
+        final Game game = Game.getInstance();
+        final GenericItem item = new GenericItemImpl();
+        final PickUpItemGameAction gameAction = new PickUpItemGameAction(item);
     }
 
     @Test
     void pickUpItem()
     {
+    }
+}
+
+class GenericItemImpl extends GenericItem
+{
+    public GenericItemImpl()
+    {
+        super(50, "TestItem", "TestItemDescription", 0, ItemType.ITEM, Rarity.LEGENDARY);
     }
 }
