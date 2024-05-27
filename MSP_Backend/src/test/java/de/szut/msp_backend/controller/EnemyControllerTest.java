f715eadc0752211efc3884b75e0164ae967e7331
Crackminer <87083544+Crackminer@users.noreply.github.com>
2024-05-27 12:23:45 +0200

    106 - CHANGE EnemyControllerTest done, cut for now due to need of merging changes from main into this branch

--- MSP_Backend/src/test/java/de/szut/msp_backend/controller/EnemyControllerTest.java
+++ MSP_Backend/src/test/java/de/szut/msp_backend/controller/EnemyControllerTest.java
@@ -1,11 +1,30 @@
 package de.szut.msp_backend.controller;
 
+import de.szut.msp_backend.Game;
+import de.szut.msp_backend.models.enemy.GenericEnemy;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EnemyControllerTest
 {
     @Test
     void getEnemy()
     {
+        ResponseEntity<?> response = new EnemyController().getEnemy("12");
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+
+        final GenericEnemy enemy = new GenericEnemy("testenemy", 20, 12);
+        final String enemyID = enemy.getID();
+
+        Game.getInstance().getMap().lake.AddEnemy(enemy);
+
+        response = new EnemyController().getEnemy(enemyID);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(enemy, response.getBody());
     }
 }
